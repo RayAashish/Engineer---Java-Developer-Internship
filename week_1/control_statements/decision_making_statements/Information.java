c9fa8a9fbbb4bfe751bc0c0d635f13f9312d6659
Aashish Kumar Ray <irealaashish@gmail.com>
2026-09-19 23:10:26 +0530

    Information about decision making statements

---
+++ week_1/control_statements/decision_making_statements/Information.java
@@ -0,0 +1,16 @@
+package week_1.control_statements.decision_making_statements;
+/**
+ * @author Aashish Kumar Ray
+ * @date 19th Sep, 2026
+ * These statements evaluate boolean expressions to decide which block of code should run.
+ * if statement: Executes a block of code only if a specified condition evaluates to true.
+ * if-else statement: Executes one block of code if the condition is true and a different block if it is false.
+ * if-else-if ladder: Checks multiple independent sequential conditions, executing the first block that evaluates to true.
+ * Nested if: Consists of an if statement placed inside another if statement for multi-level conditional checks.
+ * switch statement: Evaluates a single expression against multiple potential constant values (cases) to execute matching blocks, offering a cleaner alternative to complex if-else ladders
+ */
+public class Information{ 
+    public static void main(String[] args) {
+        
+    }
+}
