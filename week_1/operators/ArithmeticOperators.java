d30b36346cb0ad1325fb1d16406869c093a6f878
Aashish Kumar Ray <irealaashish@gmail.com>
2026-09-22 22:24:47 +0530

    Arithmetic Operations

--- week_1/operators/ArithmeticOperators.java
+++ week_1/operators/ArithmeticOperators.java
@@ -1,6 +1,20 @@
 package week_1.operators;
 
 public class ArithmeticOperators {
+
+    /**
+     * @param a
+     * @param b 
+     * Most important thing to remember is that int
+     * divsion operation, the value tends towards 0(zero)
+     * instead of floor value
+     */
+    private static void speicalDivisionRules(int a, int b){
+        System.out.println(- 5.0 / 0); // -Infinity
+        System.out.println(5.0 / 0); // Infinity
+        System.out.println(b / 2.0); // One operand is double so the result will also be a double
+        System.out.println(a / 0); // ArithmeticException
+    }
     public static void main(String[] args) {
         int a = 10, b = 7;
 
@@ -9,6 +23,7 @@ public class ArithmeticOperators {
         System.out.println("Multipication : " + (a * b)); // multipication
         System.out.println("Division : " + (a / b)); // division
         System.out.println("Remainder : " + (a % b)); //to get modulo/remainder 
+        speicalDivisionRules(a, b);
 
     }
 }
