4e90e8337cf8afb9bccb41ad41aa5eaaf47ee3fe
Aashish Kumar Ray <irealaashish@gmail.com>
2026-09-22 22:18:11 +0530

    Arithmetic Operators

---
+++ week_1/operators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package week_1.operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 7;
+
+        System.out.println("Addition : " + (a + b)); // addition
+        System.out.println("Subtraction : " + (a - b)); // subtraction
+        System.out.println("Multipication : " + (a * b)); // multipication
+        System.out.println("Division : " + (a / b)); // division
+        System.out.println("Remainder : " + (a % b)); //to get modulo/remainder 
+
+    }
+}
