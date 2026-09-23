b6876361954747318a57abd6bc062b52dedaae0f
Aashish Kumar Ray <irealaashish@gmail.com>
2026-09-23 23:42:43 +0530

    Assignment Operators

---
+++ week_1/operators/AssignmentOperators.java
@@ -0,0 +1,25 @@
+package week_1.operators;
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 19, b = 12;
+        a += b;
+        System.out.println(a);
+        a -= b;
+        System.out.println(a);
+        a /= b;
+        System.out.println(a);
+        a *= b;
+        System.out.println(a);
+        a %= b;
+        System.out.println(a);
+        a ^= b;
+        System.out.println(a);
+        a >>= b;
+        System.out.println(a);
+        a <<= b;
+        System.out.println(a);
+        a >>>= b;
+        System.out.println(a);
+    }
+}
