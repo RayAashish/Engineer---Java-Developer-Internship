4d8e43cb882caec068339d7a37488ab6ae94eb2f
Aashish Kumar Ray <irealaashish@gmail.com>
2026-09-23 23:45:57 +0530

    Relational Operators

---
+++ week_1/operators/RelationalOperators.java
@@ -0,0 +1,19 @@
+package week_1.operators;
+
+public class RelationalOperators {
+    /**
+     * @param args
+     * always return boolean value
+     */
+    public static void main(String[] args) {
+        int a, b;
+        a = b = 10;
+        int c = 17;
+        System.out.println(a == b); //true
+        System.out.println(a != b); //false
+        System.out.println(a > b); //false
+        System.out.println(a >= b); // true
+        System.out.println(a < b); // false
+        System.out.println(a <= b); // true
+    }
+}
