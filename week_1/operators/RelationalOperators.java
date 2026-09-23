f12a9e0f2ce3bb867a9b6832bdf31af57003cbea
Aashish Kumar Ray <irealaashish@gmail.com>
2026-09-23 23:53:15 +0530

    Relational Operators Tricky Questions

--- week_1/operators/RelationalOperators.java
+++ week_1/operators/RelationalOperators.java
@@ -1,6 +1,24 @@
 package week_1.operators;
 
 public class RelationalOperators {
+    private static void someTrickyPoints(){
+        // Cache Gotcha
+        Integer a = 127;
+        Integer b = 127;
+        System.out.println(a == b);// returns true
+        a = 128;
+        b = 128;
+        System.out.println(a == b); //returns false
+        String x = "hi";
+        String y = "hi";
+        String z = new String("hi");
+        System.out.println(x == y); // return true because x & y both are pointing to the same object
+        System.out.println(x == z); // returns false becase of new keyword used in creation of new object
+        //Value of x, y, x is same but the reference is different
+        //If we want to compare the value then we should use .equals() method
+        System.out.println(x.equals(y)); //returns true
+        System.out.println(x.equals(z)); // returns true
+    }
     /**
      * @param args
      * always return boolean value
@@ -8,12 +26,12 @@ public class RelationalOperators {
     public static void main(String[] args) {
         int a, b;
         a = b = 10;
-        int c = 17;
-        System.out.println(a == b); //true
-        System.out.println(a != b); //false
-        System.out.println(a > b); //false
-        System.out.println(a >= b); // true
-        System.out.println(a < b); // false
-        System.out.println(a <= b); // true
+        // System.out.println(a == b); //true
+        // System.out.println(a != b); //false
+        // System.out.println(a > b); //false
+        // System.out.println(a >= b); // true
+        // System.out.println(a < b); // false
+        // System.out.println(a <= b); // true
+        someTrickyPoints();
     }
 }
